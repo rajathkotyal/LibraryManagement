5d3a2a6fbb92902e793c8a30c6210fe3284ab7c0
rajath <rajathkotyal@gmail.com>
2020-09-07 21:58:57 +0530

    first

---
+++ booksavailable.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Books Available</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>Book Id</th>
+<th>Book Title</th>
+<th> Book Author </th>
+<th> Total Books </th>
+<th> Issued Books </th>
+<th> Remaining Books </th> 
+<th> Phone_no </th>  
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "library_bms");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+ 
+$sql = "call find2()";    //stored procedure
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["bookid"]. "</td><td>" . $row["title"] . "</td><td>" . $row["author_name"]. "</td> <td>" . $row["totalbooks"]. "</td><td>" . $row["issuedbooks"]. "</td><td>" . $row["remainingbooks"]. "</td><td>"
+. $row["phone"]. "</td></tr>";
+}//. $row["publisher"]. "</td><td>"
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+include('login.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+header("location: profile.php"); // Redirecting To Profile Page
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title> Admin Login</title>
+<link rel="stylesheet" href="/asset/css/masterlogin.css">
+</head>
+<body>
+	<style type="text/css">
+	body {
+  margin: 0;
+  padding: 0;
+  background: url(asset/img/login1.jfif) no-repeat center top;
+  background-size: cover;
+  font-family: sans-serif;
+  height: 100vh;
+}
+
+.login-box {
+  width: 500px;
+  height: 480px;
+  background: #000;
+  color: #fff;
+  top: 50%;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  box-sizing: border-box;
+  padding: 70px 30px;
+}
+
+.login-box .avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  position: absolute;
+  top: -50px;
+  left: calc(50% - 50px);
+}
+
+.login-box h1 {
+  margin: 0;
+  padding: 0 0 20px;
+  text-align: center;
+  font-size: 22px;
+}
+
+.login-box label {
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
+  display: block;
+}
+
+.login-box input {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.login-box input[type="text"], .login-box input[type="password"] {
+  border: none;
+  border-bottom: 1px solid #fff;
+  background: transparent;
+  outline: none;
+  height: 40px;
+  color: #fff;
+  font-size: 16px;
+}
+
+.login-box input[type="submit"] {
+  border: none;
+  outline: none;
+  height: 40px;
+  background: #b80f22;
+  color: #fff;
+  font-size: 18px;
+  border-radius: 20px;
+}
+
+.login-box input[type="submit"]:hover {
+  cursor: pointer;
+  background: #ffc107;
+  color: #000;
+}
+
+.login-box a {
+  text-decoration: none;
+  font-size: 12px;
+  line-height: 20px;
+  color: darkgrey;
+}
+
+.login-box a:hover {
+  color: #fff;
+}
+</style>
+<div class="login-box">
+<center>
+<h2>BMSCE Library Management</h2> <br>
+<h3>Admin Login</h3><br></center>
+<form action="" method="post">
+<label>Username :</label>
+<input id="name" name="username" placeholder="enter your username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password"><br><br>
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</body>
